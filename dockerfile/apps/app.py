cefecf09fcd12a51375e637aa62620faa7dcd5e3
Meghana Tera <mtera@andrew.cmu.edu>
2021-11-01 21:06:30 -0400

    New Version

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -17,43 +17,26 @@ def predict():
      age = request.args.get('age')
      absences = request.args.get('absences')
      health = request.args.get('health')
-     school = request.args.get('school')
-     sex = request.args.get('sex')
-     address = request.args.get('address')
-     famsize = request.args.get('famsize')
-     Pstatus = request.args.get('Pstatus')
-     Mjob = request.args.get('Mjob')
-     Fjob = request.args.get('Fjob')
-     reason = request.args.get('reason')
-     guardian = request.args.get('guardian')
-     schoolsup = request.args.get('schoolsup')
-     famsup = request.args.get('famsup')
-     paid = request.args.get('paid')
-     activities = request.args.get('activities')
-     nursery = request.args.get('nursery')
-     higher = request.args.get('higher')
-     internet = request.args.get('internet')
-     romantic = request.args.get('romantic')
-     data = [[age],[health],[absences], [school], [sex], [address], [famsize], [Pstatus], [Mjob], [Fjob], [reason], 
-            [guardian], [schoolsup], [famsup], [paid], [activities], [nursery], [higher], [internet], [romantic]]
+     Medu = request.args.get('Medu')
+     Fedu = request.args.get('Fedu')
+     traveltime = request.args.get('traveltime')
+     failures = request.args.get('failures')
+     studytime = request.args.get('studytime')
+     famrel = request.args.get('famrel')
+     freetime = request.args.get('freetime')
+     goout = request.args.get('goout')
+     Dalc = request.args.get('Dalc')
+     Walc = request.args.get('Walc')
+     G1 = request.args.get('G1')
+     G2 = request.args.get('G2')
      query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences),
-                                'school': pd.Series(school), 'sex': pd.Series(sex), 'address': pd.Series(address),
-                                'famsize': pd.Series(famsize), 'Pstatus': pd.Series(Pstatus), 'Mjob': pd.Series(Mjob), 
-                                'Fjob': pd.Series(Fjob), 'reason': pd.Series(reason), 'guardian': pd.Series(guardian), 
-                                'schoolsup': pd.Series(schoolsup), 'famsup': pd.Series(famsup), 'paid': pd.Series(paid), 
-                                'activities': pd.Series(activities), 'nursery': pd.Series(nursery), 'higher': pd.Series(higher), 
-                                'internet': pd.Series(internet), 'romantic': pd.Series(romantic)})
+                                'Medu': pd.Series(Medu), 
+                                'Fedu': pd.Series(Fedu), 'traveltime': pd.Series(traveltime), 'failures': pd.Series(failures),
+                                'studytime': pd.Series(studytime), 'famrel': pd.Series(famrel), 'freetime': pd.Series(freetime),
+                                'goout': pd.Series(goout), 'Dalc': pd.Series(Dalc), 'Walc': pd.Series(Walc), 'G1': pd.Series(G1), 
+                                'G2': pd.Series(G2)})
     
-     Names = ['school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic']
-     for col in Names: 
-        jobs_encoder = LabelBinarizer()
-        jobs_encoder.fit(query_df[col])
-        transformed = jobs_encoder.transform(query_df[col])
-        ohe_df = pd.DataFrame(transformed)
-        df = pd.concat([query_df, ohe_df], axis=1).drop([col], axis=1)
-
-     query = pd.get_dummies(query_df)
-     prediction = clf.predict(query)
+     prediction = clf.predict(query_df)
      return jsonify(np.asscalar(prediction))
 
 if __name__ == '__main__':
